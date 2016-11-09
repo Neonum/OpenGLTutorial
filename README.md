73fe97aac016d32c28a6095905acb7373dfba82b
Anton Abrosimov <git.neonum@gmail.com>
2016-11-09 21:58:29 +0200

    Model, View and Projection Matrices

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Beginners OpenGL Tutorials by ThinMatrix
 
-### Tutorial 06: Texturing
+### Tutorial 08: Model, View & Projection Matrices
 
-Video at the current part of the code: [link](https://youtu.be/SPt-aogu72A?list=PLRIWtICgwaX0u7Rf9zkZhLoLuZVfUksDP)
+Video at the current part of the code: [link](https://youtu.be/50Y9u7K0PZo?list=PLRIWtICgwaX0u7Rf9zkZhLoLuZVfUksDP)
+
+Cube vertices: [link](https://www.dropbox.com/s/xcq0xa3yqr4vmh8/cube%20vertices%20data.txt?dl=0)
+
+Projection Matrix Explained: [link](http://www.songho.ca/opengl/gl_projectionmatrix.html)
 
 ---
 
--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -20,33 +20,88 @@ public class MainGameLoop {
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
 		
-		float[] vertices = {
-			-0.5f, 0.5f, 0f,
-			-0.5f, -0.5f, 0f,
-			0.5f, -0.5f, 0f,
-			0.5f, 0.5f, 0f,
+		float[] vertices = {			
+			-0.5f,0.5f,-0.5f,	
+			-0.5f,-0.5f,-0.5f,	
+			0.5f,-0.5f,-0.5f,	
+			0.5f,0.5f,-0.5f,		
+			
+			-0.5f,0.5f,0.5f,	
+			-0.5f,-0.5f,0.5f,	
+			0.5f,-0.5f,0.5f,	
+			0.5f,0.5f,0.5f,
+			
+			0.5f,0.5f,-0.5f,	
+			0.5f,-0.5f,-0.5f,	
+			0.5f,-0.5f,0.5f,	
+			0.5f,0.5f,0.5f,
+			
+			-0.5f,0.5f,-0.5f,	
+			-0.5f,-0.5f,-0.5f,	
+			-0.5f,-0.5f,0.5f,	
+			-0.5f,0.5f,0.5f,
+			
+			-0.5f,0.5f,0.5f,
+			-0.5f,0.5f,-0.5f,
+			0.5f,0.5f,-0.5f,
+			0.5f,0.5f,0.5f,
+			
+			-0.5f,-0.5f,0.5f,
+			-0.5f,-0.5f,-0.5f,
+			0.5f,-0.5f,-0.5f,
+			0.5f,-0.5f,0.5f				
 		};
 		
-		int[] indeces = {
-			0, 1, 3,
-			3, 1, 2
+		float[] textureCoords = {				
+			0,0,
+			0,1,
+			1,1,
+			1,0,			
+			0,0,
+			0,1,
+			1,1,
+			1,0,			
+			0,0,
+			0,1,
+			1,1,
+			1,0,
+			0,0,
+			0,1,
+			1,1,
+			1,0,
+			0,0,
+			0,1,
+			1,1,
+			1,0,
+			0,0,
+			0,1,
+			1,1,
+			1,0				
 		};
 		
-		float[] textureCoords = {
-			0, 0,
-			0, 1,
-			1, 1,
-			1, 0
+		int[] indices = {
+			0,1,3,	
+			3,1,2,	
+			4,5,7,
+			7,5,6,
+			8,9,11,
+			11,9,10,
+			12,13,15,
+			15,13,14,	
+			16,17,19,
+			19,17,18,
+			20,21,23,
+			23,21,22
 		};
 		
-		RawModel model = loader.loadToVAO(vertices, textureCoords, indeces);
+		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
 		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("image")));
 		Entity entity = new Entity(staticModel, new Vector3f(0, 0, -2), 0, 0, 0, 1);
 		
 		Camera camera = new Camera();
 		
 		while (!Display.isCloseRequested()) {
-			entity.increaseRotation(0, 1, 0);
+			entity.increaseRotation(1, 1, 1);
 			camera.move();
 			renderer.prepare();
 			shader.start();
